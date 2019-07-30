58e1f605f10f661258bbea82c8e885ccc2cbc576
MFA571 <fakhar571@gmail.com>
2019-07-31 00:16:46 +0500

    updated db code

--- database.py
+++ database.py
@@ -130,4 +130,4 @@ if __name__ == "__main__":
                             sql_insert_no_parent(comment_id, parent_id, body, subreddit, created_utc, score)
 
             if row_counter % 100000 == 0:
-                print("Total rows read: {}, Paired rows: {}, Time: {}").format(row_counter, paired_rows, str(datetime.now()))
+                print("Total rows read: {}, Paired rows: {}, Time: {}".format(row_counter, paired_rows, str(datetime.now())))
